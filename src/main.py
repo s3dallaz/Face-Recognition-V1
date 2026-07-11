48465863ff5e62e3931589bdf33468340df20e73
Abdelrahman Mohamed Saadallah <abdelrahman.pro9@gmail.com>
2026-07-11 15:52:21 +0300

    Complete real-time face recognition system

--- src/main.py
+++ src/main.py
@@ -27,22 +27,31 @@ while True:
 
     # Recognition
 
-    for encode in encodings:
+    location_connector = zip(encodings , face_locations)
+    
+    frame = draw_rectangle(frame, face_locations)
+
+    for encode , location in location_connector:
+
+        top, right, bottom, left = location
 
         name = recognize_face(encode , known_encodings , known_names , threshold= 0.6)
 
-        print(name) 
+        cv.putText(
+            frame,
+            name,
+            (left, top-30),
+            fontFace= cv.FONT_HERSHEY_SIMPLEX,
+            fontScale= 0.7,
+            color= (0,255,0),
+            thickness= 2
+            )
 
-        frame = draw_rectangle(frame, face_locations)
 
-        cv.imshow("Frame", frame)
-
-
+    cv.imshow("Frame", frame)  
+  
     if cv.waitKey(1) == 27:
         break
-
-    
-
 
 
 cap.release()
